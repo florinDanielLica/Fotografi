a949de9b8900046609a277b9fffc3269e5b80992
florinDanielLica <florinlicai@gmail.com>
2021-03-20 19:45:21 +0000

    web app

---
+++ android/app/src/main/kotlin/com/website_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.website_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: WebView(
+            initialUrl: 'https://florinphotography.carrd.co/',
+            javascriptMode: JavascriptMode.unrestricted,
+          ),
+        ),
+      ),
+    );
+  }
+}
